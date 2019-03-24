351066aecbcf157d2a60c4b2c06c3da9168d637a
Jhon Vasquez <jhon.vasquez@symphony.com>
2019-03-24 12:46:57 -0300

    adding async add and fetch todos api calls handling and error handling

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,35 +1,40 @@
-import { v4 } from 'node-uuid';
 import * as api from '../api'
 import { getIsFetching } from '../reducers';
 
 // Action creators
-const receiveTodos = (response, filter) => ({
-    type: 'RECEIVE_TODOS',
-    response,
-    filter
-})
-
-const requestTodos = (filter) => ({
-    type: 'REQUEST_TODOS',
-    filter
-})
-
 export const fetchTodos = (filter) => (dispatch, getState) => {
     if (getIsFetching(getState(), filter)) {
-        console.log('loading in progress!')
         return Promise.resolve()
     }
 
-    dispatch(requestTodos(filter))
+    dispatch({
+        type: 'FETCH_TODOS_REQUEST',
+        filter
+    })
     
-    return api.fetchTodos(filter).then(todos => 
-        dispatch(receiveTodos(todos, filter))
-    )
+    return api.fetchTodos(filter).then(
+    todos => {
+        dispatch({
+            type: 'FETCH_TODOS_SUCCESS',
+            todos,
+            filter
+        })
+    },
+    error => {
+        dispatch({
+            type: 'FETCH_TODOS_FAILURE',
+            filter,
+            message: error.message || 'Error fetching todos!'
+        })
+    })
 }
 
-export const addTodo = (text) => ({
-    type: 'ADD_TODO',
-    id: v4(),
-    text
-})
+export const addTodo = (text) => (dispatch) => {
+    api.addTodo(text).then(todo => {
+        dispatch({
+            type: 'ADD_TODO_SUCCESS',
+            todo
+        })
+    })
+}
 export const toggleTodo = (id) => ({ type: 'TOGGLE_TODO', id   })
--- src/reducers/byId.js
+++ src/reducers/byId.js
@@ -1,11 +1,16 @@
 const byId = (state = {}, action) => {
   switch (action.type) {
-    case 'RECEIVE_TODOS':
+    case 'FETCH_TODOS_SUCCESS':
       const nextState = {...state}
-      action.response.forEach(todo => {
+      action.todos.forEach(todo => {
         nextState[todo.id] = todo
       });
       return nextState
+    case 'ADD_TODO_SUCCESS':
+      return {
+        ...state,
+        [action.todo.id]: action.todo,
+      }
     default:
       return state;
   }
