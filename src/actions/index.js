2b5ec7cd91814cc97cbc1763ee2116cb2f47790b
Jhon Vasquez <jhon.vasquez@symphony.com>
2019-03-25 16:12:35 -0300

    normalizaded api responses using normalize and added toggle todo api call action and reducer code to handle it

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { normalize } from 'normalizr'
+import * as schema from './schema'
 import * as api from '../api'
 import { getIsFetching } from '../reducers';
 
@@ -13,10 +15,10 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
     })
     
     return api.fetchTodos(filter).then(
-    todos => {
+    response => {
         dispatch({
             type: 'FETCH_TODOS_SUCCESS',
-            todos,
+            response: normalize(response, schema.arrayOfTodos),
             filter
         })
     },
@@ -30,11 +32,20 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
 }
 
 export const addTodo = (text) => (dispatch) => {
-    api.addTodo(text).then(todo => {
+    api.addTodo(text).then(response => {
         dispatch({
             type: 'ADD_TODO_SUCCESS',
-            todo
+            response: normalize(response, schema.todo)
         })
     })
 }
-export const toggleTodo = (id) => ({ type: 'TOGGLE_TODO', id   })
+
+export const toggleTodo = (id) => (dispatch) => {
+    api.toggleTodo(id).then(response => {
+        dispatch({
+            type: 'TOGGLE_TODO_SUCCESS',
+            response: normalize(response, schema.todo),
+        })
+    }) 
+       
+}
